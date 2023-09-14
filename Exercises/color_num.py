2a06f66cb0e939b28bdb5b1f32883597d584d157
Mar-kav <97294723+Mar-kav@users.noreply.github.com>
2023-09-14 17:13:03 +0200

    modified:   Exercises/color_num.py

--- Exercises/color_num.py
+++ Exercises/color_num.py
@@ -4,11 +4,11 @@ import string
 from colorama import Back, Style
 
 for start in range (len (text)): 
-    nextind = start+1    
-    while nextind < len(text): 
-        if text[start] not in string.digits or text[nextind] not in string.digits:
+    end = start+1    
+    while end < len(text): 
+        if (text[start] or text[end]) not in string.digits:
             break
-        if text [start] == text [nextind]:
-            print(f"\n{text[:start]}{Back.GREEN}{text [start:nextind+1]}{Style.RESET_ALL}{text[nextind:]}")
+        if text [start] == text [end]:
+            print(f"\n{text[:start]}{Back.GREEN}{text [start:end+1]}{Style.RESET_ALL}{text[end:]}")
             break
-        nextind += 1
+        end += 1
