567997f4a1897d772612e58f3351e9668bf49ebd
Mar-kav <97294723+Mar-kav@users.noreply.github.com>
2023-09-15 18:41:59 +0200

    deleted:    Exercises/Classification accuracy 2.py deleted:    Exercises/Line.py deleted:    Exercises/classification accuracy.py new file:   Labs/Lab 2/Lab2_PichuPikachuAlg.ipynb modified:   Labs/Lab 2/datapoints.txt deleted:    Lectures/Lecture 1209.ipynb

--- Exercises/Classification accuracy 2.py
+++
@@ -1,6 +0,0 @@
-tp = int(input ("How many TRUE POSITIVE predictions have been made? "))
-fp = int(input ("How many FALSE POSITIVE predictions have been made? "))
-fn = int(input ("How many FALSE NEGATIVE predictions have been made? "))
-tn = int(input ("How many TRUE NEGATIVE predictions have been made? "))
-accuracy = ((tp+tn)/(tp+tn+fp+fn))*100
-print (f"The model's accuracy is {'%.1f' % accuracy}%")
--- Exercises/classification accuracy.py
+++
@@ -1,4 +0,0 @@
-total_num = int(input ("How many predictions have been made?"))
-correct_predict = int(input ("How many of them have been correct?"))
-accuracy = correct_predict/total_num*100
-print (f"The model's accuracy is {'%.1f' % accuracy}%")
